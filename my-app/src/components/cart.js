21e50bd7e50c36aac9071ae72f10db741474e1f7
Ritik Mittal <mittalritik30@gmail.com>
2024-05-18 13:52:57 +0530

    project

--- my-app/src/components/cart.js
+++ my-app/src/components/cart.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './productstyle.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToCart, removeFromCart } from '../features/appSlice'
@@ -7,20 +7,57 @@ import Product from './product'
 
 
 const Cart = () => {
-const {cart} = useSelector(state => state.app)
-  return (
-    <div>
-        <h1>Cart</h1>
-        <Link to='/'>Back to Products</Link>
-        {
-            cart.map((item) => {
-                return (
-                  <Product item={item} page="cart" />
+    const { cart } = useSelector(state => state.app)
+
+    const [totalSum, setTotalSum] = useState(0)
+
+    useEffect(() => {
+        calculateSum()
+    }, [cart])
+
+
+    const calculateSum = () => {
+        const sumCart = cart.reduce((sum, item) => {
+            sum = sum + (item.price * item.quantity)
+            return sum
+        }, 0)
+
+        setTotalSum(sumCart)
+    }
+    return (
+        <div>
+            <h1>Cart</h1>
+            <Link to='/'>Back to Products</Link>
+
+            {
+                cart.length === 0 ? (
+                    <div>You dont have any items in your cart</div>
+                ) : (
+                    <div>
+                        {
+                            cart.map((item) => {
+                                return (
+                                    <Product item={item} page="cart" />
+                                )
+                            })
+                        }
+                    </div>
                 )
-            })
-        }
-    </div>
-  )
+            }
+
+            <div style={{
+                height: 20,
+                padding: 8,
+                backgroundColor: 'cyan',
+                color: 'black',
+                margin: 20,
+                fontSize: 18,
+                fontWeight: 'bold'
+            }}>
+                total price of your cart is: ${totalSum}
+            </div>
+        </div>
+    )
 }
 
 export default Cart
